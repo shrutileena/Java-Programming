52017fd85a377607439e22e90181657683212288
Shruti <shrutileena@gmail.com>
2023-11-14 16:20:38 +0530

    new program

---
+++ Programs/gfgPattern.java
@@ -0,0 +1,23 @@
+public class gfgPattern {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0; i<n; i++){
+            for(int j=n-1; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int k=0; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<n; i++){
+            for(int k=0; k<i; k++){
+                System.out.print(" ");
+            }
+            for(int j=n; j>i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
